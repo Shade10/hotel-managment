662b8f02c1ef3a2b1077d945d970cd9757987100
Marcin Kass <marcin.kass10@gmail.com>
2018-11-12 11:46:43 +0100

    added new navlink to room list view

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,34 +2,38 @@ import React, { Component } from "react";
 import "./App.css";
 import { Route, NavLink, withRouter } from "react-router-dom";
 import HomeView from "../HomeView/HomeView";
-import RoomsView from '../RoomsView/RoomsView'
+import RoomsView from "../RoomsView/RoomsView";
 // import { Button, Modal, Header, Image } from "semantic-ui-react";
 
 class App extends Component {
   render() {
-    return <div className="App">
+    return (
+      <div className="App">
         <header className="App-header">
           <div className="nav">
-            <div className="navigation" />
-            <ul>
-              <li>
-                <NavLink exact to="/">
-                  Strona Główna
-                </NavLink>
-              </li>
-            <li>
-              <NavLink exact to="/Room-View">
-                Pokoje
-                </NavLink>
-            </li>
-            </ul>
+            <div className="navigation">
+              <ul>
+                <li>
+                  <NavLink exact to="/">
+                    Strona Główna
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink exact to="/Room-View">
+                    Pokoje
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
           </div>
+
           <div className="route">
             <Route exact path="/" component={() => <HomeView />} />
             <Route exact path="/Room-View" component={() => <RoomsView />} />
           </div>
         </header>
-      </div>;
+      </div>
+    );
   }
 }
 
