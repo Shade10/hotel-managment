a548d580037a5af4fa5ba623bd0f4612b1247fb7
Marcin Kass <marcin.kass10@gmail.com>
2018-11-12 12:00:46 +0100

    prepared firebase setup

---
+++ src/setup.firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+
+var config = {
+    apiKey: "AIzaSyCyrESV2X17o7xAnjhALo_N0d9tgd4Za2Q",
+    authDomain: "hotel-managment-4e09d.firebaseapp.com",
+    databaseURL: "https://hotel-managment-4e09d.firebaseio.com",
+    projectId: "hotel-managment-4e09d",
+    storageBucket: "hotel-managment-4e09d.appspot.com",
+    messagingSenderId: "138097369394"
+  };
+  firebase.initializeApp(config);
