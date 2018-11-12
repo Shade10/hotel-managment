6533fa9c39e613e79bf09ed0e0c1e4c679b2ec1c
Marcin Kass <marcin.kass10@gmail.com>
2018-11-12 10:09:16 +0100

    added semantic ui module and first navLink to home page

---
+++ src/components/HomeView/HomeView.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import './HomeView.css'
+
+class HomeView extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return  (
+      <div className="HomeView">
+        HomeView
+      </div>
+    )
+  }
+}
+
+export default HomeView
