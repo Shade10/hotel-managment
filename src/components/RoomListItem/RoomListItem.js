27fc23a76f6b37443c6cf4fd47a1248fbcbe33ae
Marcin Kass <marcin.kass10@gmail.com>
2018-11-12 09:23:58 +0100

    preparing for create rooms list and navigations

---
+++ src/components/RoomListItem/RoomListItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import './RoomListItem.css'
+
+class RoomListItem extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return  (
+      <div className="RoomListItem">
+        RoomListItem
+      </div>
+    )
+  }
+}
+
+export default RoomListItem
---
+++ src/components/RoomsList/RoomsList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import './RoomsList.css'
+
+class RoomsList extends Component {
+  static propTypes = {
+
+  }
+
+  render() {
+    return  (
+      <div className="RoomsList">
+        RoomsList
+      </div>
+    )
+  }
+}
+
+export default RoomsList
