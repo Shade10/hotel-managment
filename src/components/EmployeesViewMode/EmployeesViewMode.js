7dcb3ffa6402825a8fbbe96bc6c42ed1e4af9ba6
Marcin Kass <marcin.kass10@gmail.com>
2018-11-22 23:00:56 +0100

    added new componet for edditing emlpoyees data and WIP: looking for minimalize code for toggle handle

---
+++ src/components/EmployeesViewMode/EmployeesViewMode.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './EmployeesViewMode.css'
+
+class EmployeesViewMode extends Component {
+
+  render() {
+    return  (
+      <div className="EmployeesViewMode">
+        EmployeesViewMode
+      </div>
+    )
+  }
+}
+
+export default EmployeesViewMode
